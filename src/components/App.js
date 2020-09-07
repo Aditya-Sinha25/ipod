c97df29f9ca6bd20ffc78205090f8b0088688f93
Aditya <adityagodofgamer.1@gmail.com>
2020-09-07 14:00:41 +0530

    File structure changed

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Ipod from './Ipod';
+
+class App extends React.Component{
+  
+  render(){
+    
+    return (
+      <div>
+        <Ipod />
+       
+      </div>
+    );
+  }
+  
+}
+
+export default App;
