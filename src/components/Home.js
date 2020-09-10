9745d3fc353e86a88c7c1ea64a61b0341127a445
Aditya <adityagodofgamer.1@gmail.com>
2020-09-10 20:59:31 +0530

    Fixing Bug

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+            <div className='screen-left'>
+              <div><h2>IPod</h2></div>
+              <div>Settings</div>
+              <div>Coverflow </div>
+              <div>Music </div>
+              <div>Games </div>
+            </div>
+            <div className='screen-right'>
+              
+            </div>
+            
+        </div>
+        );
+    }
+}
+
+export default Home;
--- src/components/Ipod.js
+++ src/components/Ipod.js
@@ -3,95 +3,55 @@ import Settings from './Settings';
 import Music from './Music';
 import Cover from './CoverFlow';
 import Game from './Game';
+import Home from './Home';
+import ZingTouch from 'zingtouch';
 class Ipod extends React.Component{
-    constructor () {
-        super();
-        this.state={
-          menu:{
-            settings:'',
-            coverflow:'',
-            games:'',
-            music:'',
-            show:false,
-          }
-        };
-      }
-      handleMenu = () =>{
-        console.log('menu clicked');
-        console.log('prev state',this.state);
-        this.setState({
-          show:true,
-          settings:'settings',
-          coverflow:'coverflow',
-          music:'music',
-          games:'games'
-        })
-        console.log('new state',this.state);
-    
-      }
-      handleClickSettings = () =>{
-        this.setState({
-          show:true,
-          settings:<Settings />,
-          coverflow:'',
-          music:'',
-          games:''
-        })
-      }
-      handleClickCover = () =>{
-        this.setState({
-          show:true,
-          settings:'',
-          coverflow:<Cover />,
-          music:'',
-          games:''
-        })
-      }
-      handleClickMusic = () =>{
-        this.setState({
-          show:true,
-          settings:'',
-          coverflow:'',
-          music:<Music />,
-          games:''
-        })
-      }
-      handleClickGames = () =>{
-        this.setState({
-          show:true,
-          settings:'',
-          coverflow:'',
-          music:'',
-          games:<Game />
-        })
-      }
+ constructor(){
+   super();
+   this.state={
+     activeState:''
+   }
+ };
+
+ //rotation = () =>{
+ // var containerElement=document.getElementsByClassName('div1');
+ // var activeRegion=ZingTouch.Region(containerElement);
+ // activeRegion.bind(containerElement,'rotate',function(e){
+ //   console.log('inside container');
+ // })
+  
+
+ //}
+
+ handleClickMenu = () =>{
+   this.setState({
+     activeState:<Home />
+   });
+
+ }
+ 
+
     render(){
-        const {show,settings,coverflow,games,music}=this.state;
+      const {activeState} =this.state;
         return(
-            <div className="App">
+            <div className="Ipod-container">
             <div className='screen'>
-            {
-              show && 
-              <div>
-                  <div className='screen-left'>
-                    <div><h2>IPod</h2></div>
-                    <div onClick={this.handleClickSettings}>{settings}</div>
-                    <div onClick={this.handleClickCover}>{coverflow} </div>
-                    <div onClick={this.handleClickMusic}>{music} </div>
-                    <div onClick={this.handleClickGames}>{games} </div>
-                  </div>
-                  <div className='screen-right'>
-                    
-                  </div>
-                  
-              </div>
-            }
+             {activeState}
           </div>
-          <div className='buttons'>
-            <div className='menu' onClick={this.handleMenu}>Menu</div>
+          <div className="wheel">
+        	  <div className="div1" >
+
+        	    <div className="div2"></div>
+        	    <div className="div3" onClick={this.handleClickMenu}>Menu</div>
+        	    <div className="div4" onCLick={this.handleClickForward}><img src="https://image.flaticon.com/icons/svg/860/860754.svg" /></div>
+        	    <div className="div5"><img src="https://image.flaticon.com/icons/svg/149/149126.svg" /></div>
+        	    <div className="div6"><img src="https://image.flaticon.com/icons/svg/39/39712.svg" /></div>
+
+        	  </div>
+
           </div>
           </div>
-        )
+        );
     }
 
 }
