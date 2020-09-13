a939aa56b12410745f6bf25ed191f6956509dfac
Aditya <adityagodofgamer.1@gmail.com>
2020-09-13 21:28:37 +0530

    Adding Different Components

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 class Home extends React.Component{
     render(){
+        const {forwardPress} =this.props;
         return(
             <div>
             <div className='screen-left'>
-              <div><h2>IPod</h2></div>
-              <div>Settings</div>
-              <div>Coverflow </div>
-              <div>Music </div>
-              <div>Games </div>
+              <h2>IPod</h2>
+              <div className="screen-left-item">Settings</div>
+              <div className="screen-left-item">Coverflow </div>
+              <div className="screen-left-item">Music </div>
+              <div className="screen-left-item">Game </div>
             </div>
             <div className='screen-right'>
               
--- src/components/Ipod.js
+++ src/components/Ipod.js
@@ -9,7 +9,9 @@ class Ipod extends React.Component{
  constructor(){
    super();
    this.state={
-     activeState:''
+     activeState:'',
+     activeList:'',
+     prevList:''
    }
  };
 
@@ -29,21 +31,57 @@ class Ipod extends React.Component{
    });
 
  }
+
+ handleClickForward = () =>{
+   const {activeList,prevList}=this.state;
+   console.log('Forward Clicked');
+   if(prevList===''){
+     this.setState({
+       prevList:'Settings',
+       activeList:<Settings />
+     })
+   }
+   else if(prevList==='Settings'){
+    this.setState({
+      activeList:<Cover />,
+      prevList:'Cover',
+    })
+  }
+  else if(prevList==='Cover'){
+    this.setState({
+      activeList:<Music />,
+      prevList:'Music',
+    })
+ }
+ console.log(activeList);
+ }
+
+ handleClickMain = () =>{
+   var {activeList,activeState} =this.state;
+   console.log("Main circle clicked");
+   this.setState({
+     activeState:activeList
+   });
+   console.log(activeState);
+   console.log(activeList);
+
+ }
  
 
     render(){
       const {activeState} =this.state;
+      console.log('render');
         return(
             <div className="Ipod-container">
             <div className='screen'>
-             {activeState}
+             {activeState }
           </div>
           <div className="wheel">
         	  <div className="div1" >
 
-        	    <div className="div2"></div>
+        	    <div className="div2" onClick={this.handleClickMain}></div>
         	    <div className="div3" onClick={this.handleClickMenu}>Menu</div>
-        	    <div className="div4" onCLick={this.handleClickForward}><img src="https://image.flaticon.com/icons/svg/860/860754.svg" /></div>
+        	    <div className="div4" onClick={this.handleClickForward}><img src="https://image.flaticon.com/icons/svg/860/860754.svg" /></div>
         	    <div className="div5"><img src="https://image.flaticon.com/icons/svg/149/149126.svg" /></div>
         	    <div className="div6"><img src="https://image.flaticon.com/icons/svg/39/39712.svg" /></div>
 
